694f7433f172cd835dea53ee50d8edf4ee834b50
Tarcisio Coutinho <tcs5cin@gmail.com>
2024-12-14 23:08:03 -0300

    computed properties

---
+++ basics-05-using-the-native-event-object/app.js
@@ -0,0 +1,32 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: ''
+    };
+  },
+  methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput() {
+      this.name = ''
+    }
+  },
+  computed: {
+    fullName() {
+      if (this.name === '') return "";
+
+      return this.name + " " + "Last Name"
+    }
+  }
+});
+
+app.mount('#events');
