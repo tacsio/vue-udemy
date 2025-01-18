8676fb80067ed059507beab33090d5dcd60dc9d0
Tarcisio Coutinho <tcs5cin@gmail.com>
2025-01-17 21:53:49 -0300

    proxy

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -16,3 +16,27 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+
+const data = {
+  message: 'Hello'
+};
+
+const handler = {
+  set(target, key, value) {
+    console.log(target);
+    console.log(key);
+    console.log(value);
+
+    if (key === 'message') {
+      target.longMessage = value + "world";
+    }
+    target.message = value;
+  }
+};
+
+const proxy = new Proxy(data, handler)
+
+proxy.message = 'Hello!!!!!!!';
+
+console.log(proxy.longMessage);
