248d269f0842d06e3dfe2337a5f3f0c0d03ffa7b
Tarcisio Coutinho <tcs5cin@gmail.com>
2025-01-21 21:39:48 -0300

    lifecycle

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -15,10 +15,41 @@ const app = Vue.createApp({
       this.message = this.$refs.userText.value;
     },
   },
+
+  //Lifecycle
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount");
+  },
+  unmount() {
+    console.log("unmount");
+  }
 });
 
 app.mount('#app');
 
+setTimeout(function () {
+  app.unmount();
+}, 3000)
+
+//app 2
 
 const app2 = Vue.createApp({
   template: `
