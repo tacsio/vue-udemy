fdb24be9801592c43909ae1a47b2f3fcb52e6d94
Tarcisio Coutinho <tcs5cin@gmail.com>
2025-01-22 19:24:14 -0300

    basic vue app

--- cmp-intro-01-starting-setup/app.js
+++ cmp-intro-01-starting-setup/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
           id: "manuel",
@@ -18,11 +17,35 @@ const app = Vue.createApp({
       ],
     };
   },
+});
+
+app.component("friend-contact", {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lopez",
+        phone: "101234 5678 991",
+        email: "manuel@localhost.com",
+      }
+    };
+  },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
   },
+  template: `
+    <li :key="friend.id">
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+    </li>
+  `,
 });
 
 app.mount("#app");
---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2> {{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ detailsVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsVisible">
+      <li><strong>Phone: </strong>{{ friend.phone }}</li>
+      <li><strong>Email: </strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsVisible: false,
+      friend: {
+        id: "julie",
+        name: "Julie Jones",
+        phone: " 09876 543 221",
+        email: "julie@localhost.com",
+      }
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsVisible = !this.detailsVisible;
+    }
+  }
+}
+</script>
