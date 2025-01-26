eca2f07fb642246ca8b4bd2e8307f606a39c8e79
Tarcisio Coutinho <tcs5cin@gmail.com>
2025-01-25 21:05:47 -0300

    component communication

---
+++ cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">{{ detailsVisible ? "Hide" : "Show" }} Details</button>
+    <ul v-if="detailsVisible">
+      <li><strong>Phone: </strong>{{ phoneNumber }}</li>
+      <li><strong>Email: </strong>{{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["name", "phoneNumber", "emailAddress"],
+  data() {
+    return {
+      detailsVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsVisible = !this.detailsVisible;
+    },
+  },
+};
+</script>
