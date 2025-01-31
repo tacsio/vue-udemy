dd5c6483469278405b4db7db706d0ea96cdda824
Tarcisio Coutinho <tcs5cin@gmail.com>
2025-01-30 22:40:13 -0300

    connection components II

--- cmp-communication-01-starting-setup/src/App.vue
+++ cmp-communication-01-starting-setup/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <section>
-    <h2>My Friends</h2>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -43,6 +46,18 @@ export default {
       const selectedFriend = this.friends.find((friend) => friend.id === friendId);
       selectedFriend.favorite = !selectedFriend.favorite;
     },
+
+    addContact(payload) {
+      const newFriend = {
+        id: new Date().toISOString(),
+        name: payload.enteredName,
+        phone: payload.enteredPhone,
+        email: payload.enteredEmail,
+        favorite: false,
+      };
+
+      this.friends.push(newFriend);
+    },
   },
 };
 </script>
@@ -53,15 +68,12 @@ export default {
 * {
   box-sizing: border-box;
 }
-
 html {
   font-family: "Jost", sans-serif;
 }
-
 body {
   margin: 0;
 }
-
 header {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 3rem auto;
@@ -73,14 +85,13 @@ header {
   width: 90%;
   max-width: 40rem;
 }
-
 #app ul {
   margin: 0;
   padding: 0;
   list-style: none;
 }
-
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -89,14 +100,12 @@ header {
   width: 90%;
   max-width: 40rem;
 }
-
 #app h2 {
   font-size: 2rem;
   border-bottom: 4px solid #ccc;
   color: #58004d;
   margin: 0 0 1rem 0;
 }
-
 #app button {
   font: inherit;
   cursor: pointer;
@@ -106,11 +115,23 @@ header {
   padding: 0.05rem 1rem;
   box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
 }
-
 #app button:hover,
 #app button:active {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
