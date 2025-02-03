2f3951a20a1dcd315865664403de453bbb9d9981
Tarcisio Coutinho <tcs5cin@gmail.com>
2025-02-03 20:15:58 -0300

    local vs global components

---
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <BadgeList/>
+    <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
+import BadgeList from './components/BadgeList.vue';
+
+export default {
+  components: {
+    "the-header": TheHeader,
+    UserInfo: UserInfo,
+    BadgeList,
+  },
+
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
