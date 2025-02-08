8c50ff18aef3698e20f5abeba21e7e30ce01bde0
Tarcisio Coutinho <tcs5cin@gmail.com>
2025-02-08 10:39:18 -0300

    scoped slots

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,21 +1,29 @@
 <template>
   <div>
     <the-header></the-header>
-    <BadgeList/>
+    <BadgeList />
     <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps['anotherProp'] }}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
 import UserInfo from "./components/UserInfo.vue";
-import BadgeList from './components/BadgeList.vue';
+import BadgeList from "./components/BadgeList.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 
 export default {
   components: {
     "the-header": TheHeader,
     UserInfo: UserInfo,
     BadgeList,
+    CourseGoals,
   },
 
   data() {
