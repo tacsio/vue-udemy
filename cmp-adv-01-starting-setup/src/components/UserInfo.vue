5b50dbf03d067b26e390f8a0488bdfa7dd0aa0ca
Tarcisio Coutinho <tcs5cin@gmail.com>
2025-02-08 10:06:56 -0300

    slots

--- cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,29 +1,24 @@
 <template>
   <section>
-    <div>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </div>
-    <p>{{ infoText }}</p>
+    <base-card>
+      <header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </header>
+      <p>{{ infoText }}</p>
+    </base-card>
   </section>
 </template>
 
 <script>
 export default {
-  props: ['fullName', 'infoText', 'role'],
+  props: ["fullName", "infoText", "role"],
 };
 </script>
 
 <style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
 
-section div {
+section header {
   display: flex;
   justify-content: space-between;
   align-items: center;
