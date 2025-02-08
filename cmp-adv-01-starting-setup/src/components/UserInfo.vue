6384ad4123171bde934fa0a14e695508862e3d86
Tarcisio Coutinho <tcs5cin@gmail.com>
2025-02-08 10:14:33 -0300

    named slots

--- cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,10 +1,10 @@
 <template>
   <section>
     <base-card>
-      <header>
+      <template v-slot:header>
         <h3>{{ fullName }}</h3>
         <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-      </header>
+      </template>
       <p>{{ infoText }}</p>
     </base-card>
   </section>
@@ -15,12 +15,3 @@ export default {
   props: ["fullName", "infoText", "role"],
 };
 </script>
-
-<style scoped>
-
-section header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-</style>
