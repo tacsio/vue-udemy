9d4fa60dda0ee7456aa039434b200c83dd4d4abb
Tarcisio Coutinho <tcs5cin@gmail.com>
2025-02-22 10:13:05 -0300

    dynamic component

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,39 +1,54 @@
 <template>
   <div>
     <the-header></the-header>
-    <BadgeList />
-    <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info>
-    <course-goals>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manager Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals> -->
+    <!-- <manage-goals v-else></manage-goals> -->
+    <component :is="selectedComponent"></component>
+    <!-- <BadgeList /> -->
+    <!-- <user-info :full-name="activeUser.name" :info-text="activeUser.description" :role="activeUser.role"></user-info> -->
+    <!-- <course-goals>
       <template #default="slotProps">
         <h2>{{ slotProps.item }}</h2>
-        <p>{{ slotProps['anotherProp'] }}</p>
+        <p>{{ slotProps["anotherProp"] }}</p>
       </template>
-    </course-goals>
+    </course-goals> -->
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import UserInfo from "./components/UserInfo.vue";
-import BadgeList from "./components/BadgeList.vue";
-import CourseGoals from "./components/CourseGoals.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
   components: {
     "the-header": TheHeader,
-    UserInfo: UserInfo,
-    BadgeList,
-    CourseGoals,
+    // UserInfo: UserInfo,
+    // BadgeList,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
   },
 
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
