3829afa34236912d85f2a31e1c3ccc63e1c30e08
Tarcisio Coutinho <tcs5cin@gmail.com>
2025-06-01 10:12:43 -0300

    base project list

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <the-header title="RememberMe"></the-header>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+
+export default {
+  components: {
+    StoredResources,
+    TheHeader
+  },
+
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide', title: 'Official Guide', description: 'The Oficial VueJS Documentation', link: 'https://vuejs.org'
+        },
+        {
+          id: 'google', title: 'Google', description: 'Lern to google...', link: 'https://www.google.com'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
